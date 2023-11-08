33bfc469958dc9a00943592e13624e8e31ecb971
Ashutosh Kumar <sinhapintu545@gmail.com>
2023-11-09 02:08:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Userinfo from './Components/Userinfo';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Userinfo />
+    </div>
+  );
+}
+
+export default App;
